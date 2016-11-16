5a624dc1a37b0aec137758062a71ca22a8d16394
Joseph Sams <jsams@Mac25.local>
2016-11-16 14:34:24 -0500

    Git Init

---
+++ src/Logging.js
@@ -0,0 +1,44 @@
+class Logging {
+    constructor(jobNumber, operator, error) {
+        this.jobNumber = jobNumber;
+        this.operator = operator;
+        this.error = error;
+    }
+
+    logger() {
+        var currentdate = new Date();
+        var datetime = currentdate.getDate() + "/"
+                        + (currentdate.getMonth()+1)  + "/"
+                        + currentdate.getFullYear() + " @ "
+                        + currentdate.getHours() + ":"
+                        + currentdate.getMinutes() + ":"
+                        + currentdate.getSeconds();
+
+          var filepath = "G33STORE-1/4_Joe/scripts/_logs/cinemark/" + this.jobNumber + ".txt";
+          var write_file = File(filepath);
+
+          if (!write_file.exists) {
+            write_file = new File(filepath);
+                  var out;
+              if (write_file !== '') {
+                out = write_file.open('w', undefined, undefined);
+                write_file.encoding = "UTF-8";
+                write_file.lineFeed = "Macintosh";
+              }
+              if (out !== false) {
+                write_file.writeln(this.operator + ' worked ' + this.jobNumber + ' at ' + datetime + '\nAny Errors: ' + this.error + '\n');
+                write_file.close();
+              }
+          } else {
+            var append_file = File(filepath);
+              append_file.open('a', undefined, undefined);
+              if (append_file !== '') {
+                append_file.writeln(this.operator + ' worked ' + this.jobNumber + ' at ' + datetime + '\nAny Errors: ' + this.error + '\n');
+
+              append_file.close();
+            }
+          }
+      }
+}
+
+export default Logging;
---
+++ src/main.js
@@ -0,0 +1,50 @@
+// ./node_modules/.bin/extendscriptr --script src/main.js --output dist/main.dist.js
+
+import SaveFiles from './SaveFiles';
+import Selections from './Selections';
+import Colors from './Colors';
+import Logging from './Logging';
+
+let doc = app.activeDocument,
+    jobnumber = prompt('Job Number: ', '123456P01');
+    panelSelection = new Selections,
+    fullWidth = parseInt(doc.width),
+    fullHeight = parseInt(doc.height),
+    res = doc.resolution,
+    overlapWidth = 2 * res,
+    numPanels = Math.ceil(fullWidth / 51),
+    panelWidth = (fullWidth / numPanels) * res,
+    error = '';
+    colors = new Colors(),
+    saveAsTif = new SaveFiles(File('G33STORE-1/WIP/' + jobnumber + '/prep_art/' + jobnumber + 'panels.tif'));
+
+    black = colors.solidColor(0, 0, 0, 100);
+    red = colors.solidColor(0, 100, 100, 0);
+
+function createMarks(layerName, opacity, color, markWidth, offsetX) {
+    let newLayer = doc.artLayers.add();
+
+    newLayer.name = layerName;
+    doc.activeLayer.fillOpacity = opacity;
+
+    for(let i = 1; i < numPanels; i ++) {
+        let x = i * offsetX,
+            y = 0,
+            width = markWidth,
+            height = fullHeight * res;
+            selectedRegion = panelSelection.selection(x, y, width, height);
+        doc.selection.select(selectedRegion);
+        doc.selection.fill(color);
+    }
+}
+try {
+    createMarks('Overlap', 50, red, overlapWidth, panelWidth);
+    createMarks('Panel Breaks', 100, black, 3, panelWidth);
+    saveAsTif.saveTIF();
+} catch(e) {
+    error += 'Line: ' + e.line.toString() + ', ' + e.name.toString() + ', ' + e.message.toString() + '. ';
+    alert('There was an error...');
+}
+
+let log = new Logging(jobnumber, 'Joe', error);
+log.logger();
